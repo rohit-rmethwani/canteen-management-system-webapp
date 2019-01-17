67e0ec81c0698e525579ca036081278dcf7fe5ad
unknown <rohit.methwani31@gmail.com>
2019-01-18 01:00:36 +0530

    Added login and register

---
+++ includes/pages/register.php
@@ -0,0 +1,48 @@
+<div class="page-header header-filter">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+                <div class="card card-login">
+                    <form class="form" method="" action="">
+                        <div class="card-header card-header-primary text-center">
+                            <h4 class="card-title">Login</h4>
+                        </div>
+                        <div class="card-body">
+
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                      <i class="material-icons">mail</i>
+                                    </span>
+                                </div>
+                                <input type="email" class="form-control" placeholder="Email...">
+                            </div>
+
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                      <i class="material-icons">lock_outline</i>
+                                    </span>
+                                </div>
+                                <input type="password" class="form-control" placeholder="Password...">
+                            </div>
+
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                      <i class="material-icons">lock_outline</i>
+                                    </span>
+                                </div>
+                                <input type="password" class="form-control" placeholder="Confirm Password...">
+                            </div>
+
+                        </div>
+                        <div class="footer text-center">
+                            <a href="" class="btn btn-primary">Login</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ user_login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include_once ("includes/header.php");
+?>
+<body class="login-page sidebar-collapse">
+<?php
+    include_once ("includes/pages/register.phps");
+?>
+<?php
+    include_once ("includes/scripts.php");
+?>
+</body>
+
+</html>
